b6f8e4c563a0df51fee70cff2a8af8f9674d8e6e
James Lee <james.lee@stiqr.com>
2010-10-12 07:57:03 -0700

    added footer sections, still the footer text and copy right needs to be added.

--- body.php
+++ body.php
@@ -15,7 +15,52 @@
 			</div>
 		</div>
 		<div id="body_content">
-			<img src="/images/graph.png" />
+			<div id="graph">
+				<img src="/images/map_icon.png">
+				<div id="graph_text">
+					<h1>List with us! We can sell it.</h1>
+					<br><br>
+					<p>Just like life itself, the real estate market exists and 
+	operates in cycles. It’s a buyers market and properties 
+	are still selling! <span class="orange">read more ></span></p>
+				</div>
+			</div>
+			<h1>Wembleys Enterprise Lorem ipsum dolor sit amet, adipisicing aquien dolore aliqua. Ut 
+enim ad , nostrud exercitation.</h1>
+		</div>
+		<div id="footer">
+			<div class="line"></div>
+			<div id="commercial" class="foot_list left">
+				<img src="/images/commercial_estate.png" class="left">
+				<h1>commercial</h1>
+				<p>real estate</p>
+			</div>
+			
+			<div class="vert_line left"></div>
+			
+			<div id="commercial_b" class="foot_list left">
+				<img src="/images/commercial_lending.png" class="left">
+				<h1>commercial</h1>
+				<p>lending</p>
+			</div>
+		
+			<div class="vert_line left"></div>
+		
+			<div id="residential" class="foot_list left">
+				<img src="/images/residential_estate.png" class="left">
+				<h1>residential</h1>
+				<p>real estate</p>
+			</div>
+			
+			<div class="vert_line left"></div>
+			
+			<div id="residential_b" class="foot_list left">
+				<img src="/images/residential_lending.png" class="left">
+				<h1>residential</h1>
+				<p>morgate & lending</p>
+			</div>
+			<div class="clear"></div>
+			<div class="line"></div>
 		</div>
 	</div>
 </div>
--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
-<div id="footer">
-</div>
+
 
 
 <script language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> <script language="javascript" src="http://free.stiqr.com/sticker/api"></script>
@@ -9,6 +8,6 @@
 <script language="javascript">
 	$(document).ready(function(){
 				$("#fb").append('<iframe src="http://www.facebook.com/plugins/like.php?href=wembleys.com&amp;layout=standard&amp;show_faces=true&amp;width=250&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:25px;" allowTransparency="true"></iframe>');
-				$("body").display();
+				$("body").show();
 	});
 </script>
