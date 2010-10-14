f179e8f3f2e2c86bd90064b1b04fc97cc5a5aaff
James Lee <james.lee@stiqr.com>
2010-10-14 05:34:11 -0700

    added page functionality for multiple pages.

--- body.php
+++ body.php
@@ -17,20 +17,9 @@
 			<div class="line">
 			</div>
 		</div>
-		<div id="body_content">
-			<div id="graph">
-				<img src="/images/map_icon.png">
-				<div id="graph_text">
-					<h1>List with us! We can sell it.</h1>
-					<br><br>
-					<p>Just like life itself, the real estate market exists and 
-	operates in cycles. It’s a buyers market and properties 
-	are still selling! <a href='#'>read more ></a></p>
-				</div>
-			</div>
-			<h1>Wembleys Enterprise Lorem ipsum dolor sit amet, adipisicing aquien dolore aliqua. Ut 
-enim ad , nostrud exercitation.</h1>
-		</div>
+		<?
+			include("pages/$page/$page.php");
+		?>
 		<div id="footer">
 			<div class="line"></div>
 			<div id="commercial" class="foot_list left">
